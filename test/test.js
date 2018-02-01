3bde9b381c1846251ca94a2a76a9a841276a6fb1
moznion <moznion@gmail.com>
2018-02-01 10:29:17 +0900

    Fix test

--- test/test.js
+++ test/test.js
@@ -5,24 +5,24 @@ import EBMessageClassifier from '../lib/index.js';
 
 describe('Danger message', () => {
   it('should be treated as danger message', () => {
-    const message = "Environment health has transitioned from Info to Degraded";
+    const message = "Environment health has transitioned from Info to Degraded.";
     assert(EBMessageClassifier.isDangerMessage(message) === true);
   });
 
   it('should not be treated as danger message', () => {
-    const message = "Environment health has transitioned from Degraded to Ok";
+    const message = "Environment health has transitioned from Degraded to Ok.";
     assert(EBMessageClassifier.isDangerMessage(message) === false);
   });
 });
 
 describe('Warning message', () => {
   it('should be treated as warning message', () => {
-    const message = "Environment health has transitioned from Ok to Info";
+    const message = "Environment health has transitioned from Ok to Info.";
     assert(EBMessageClassifier.isWarningMessage(message) === true);
   });
 
   it('should not be treated as warning message', () => {
-    const message = "Environment health has transitioned from Info to Degraded";
+    const message = "Environment health has transitioned from Info to Degraded.";
     assert(EBMessageClassifier.isWarningMessage(message) === false);
   });
 });
